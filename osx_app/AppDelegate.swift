ff08cc94f27d467f5be6822c5da666eedd4752c3
Rostislav Jadavan <rostislav.jadavan@gmail.com>
2018-06-17 16:17:30 +0200

    Initial Commit

---
+++ osx_app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  osx_app
+//
+//  Created by Rostislav Jadavan on 17/06/2018.
+//  Copyright © 2018 Rostislav Jadavan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ osx_app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  osx_app
+//
+//  Created by Rostislav Jadavan on 17/06/2018.
+//  Copyright © 2018 Rostislav Jadavan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
